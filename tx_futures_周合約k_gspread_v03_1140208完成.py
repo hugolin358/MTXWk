8e06f6afee170b73b515d5abe4c31fe985ba3615
hugolin358 <hugolin26@gmail.com>
2025-02-08 22:21:09 +0800

    Update tx_futures_周合約k_gspread_v03_1140208完成.py
    delete plot part and import google.colab

--- tx_futures_周合約k_gspread_v03_1140208完成.py
+++ tx_futures_周合約k_gspread_v03_1140208完成.py
@@ -108,7 +108,7 @@ while i < len(df):  # Iterate through all rows
 contractExtract_data=wednesdays_data
 contractExtract_data
 
-from google.colab import files
+#from google.colab import files
 
 #wednesdays_data.to_csv('output.csv')
 #files.download('output.csv')
@@ -293,40 +293,3 @@ def trigger_gas_webapp():
 if __name__ == "__main__":
     trigger_gas_webapp()
 
-"""10 plotting"""
-
-import plotly.graph_objects as go
-
-
-
-# Assuming you have the 'new_df' DataFrame with the calculated values
-
-fig = go.Figure(data=[go.Candlestick(
-    x=new_df['開倉日期'],  # Assuming "開倉日期" is the date column
-    open=new_df['開盤'],    # Assuming "開盤" is the opening price column
-    high=new_df['最高'],    # Assuming "最高" is the highest price column
-    low=new_df['最低'],     # Assuming "最低" is the lowest price column
-    close=new_df['收盤'],    # Assuming "收盤" is the closing price column
-    increasing_line_color='red',
-    decreasing_line_color='green')])
-
-fig.update_layout(title_text='台指期貨月Ｋ線圖')  # Set the title
-fig.show()
-import plotly.graph_objects as go
-
-# Assuming you have the 'new_df' DataFrame with the calculated values
-
-fig = go.Figure(data=[go.Candlestick(
-    x=new_df['開倉日期'],  # Assuming "開倉日期" is the date column
-    open=new_df['開盤'],    # Assuming "開盤" is the opening price column
-    high=new_df['最高'],    # Assuming "最高" is the highest price column
-    low=new_df['最低'],     # Assuming "最低" is the lowest price column
-    close=new_df['收盤'],    # Assuming "收盤" is the closing price column
-    increasing_line_color='red',
-    decreasing_line_color='green')])
-
-fig.update_layout(title_text='台指期貨周Ｋ線圖')  # Set the title
-fig.show()
-
-fig.update_layout(title_text='台指期貨周Ｋ線圖')  # Set the title
-fig.show()
