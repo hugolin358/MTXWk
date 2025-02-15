22bbf51cd6600566a3952ab27b2050006dba46ce
hugolin358 <hugolin26@gmail.com>
2025-02-15 10:27:40 +0800

    Update tx月合約全日k_gspread__v02_1140215_完成.py
    eliminate trigger GAS part because it would trigger in main yml

--- tx月合約全日k_gspread__v02_1140215_完成.py
+++ tx月合約全日k_gspread__v02_1140215_完成.py
@@ -354,27 +354,6 @@ worksheet.clear()
 # Write the DataFrame to the sheet
 set_with_dataframe(worksheet, new_df2)
 
-"""9. To trigger GAS web app to write data from weekly to 周 sheet of 關卡價"""
-
-import requests
-
-def trigger_gas_webapp():
-    # The URL of the GAS web app
-    url = "https://script.google.com/macros/s/AKfycbxFLi4_yhfdsDGnEAYuqrj1aCbx8izUuWjtM-7pyQOnRUToiHfY6-e6FA3yQjKjCl47Cw/exec"
-
-    try:
-        # Send a GET request to the GAS web app
-        response = requests.get(url)
-
-        # Check the response status code
-        if response.status_code == 200:
-            print("Request successful!")
-            print("Response from GAS:", response.text)
-        else:
-            print(f"Request failed with status code {response.status_code}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
 if __name__ == "__main__":
     trigger_gas_webapp()
 
